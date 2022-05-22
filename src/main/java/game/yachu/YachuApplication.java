aea75c70ecbfc04588966cc50c318958c62df62b
timel2ss <dyddn0823@hanyang.ac.kr>
2022-05-22 22:09:58 +0900

    Initial Commit

---
+++ src/main/java/game/yachu/YachuApplication.java
@@ -0,0 +1,12 @@
+package game.yachu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YachuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YachuApplication.class, args);
+	}
+}
---
+++ src/test/java/game/yachu/YachuApplicationTests.java
@@ -0,0 +1,13 @@
+package game.yachu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YachuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
